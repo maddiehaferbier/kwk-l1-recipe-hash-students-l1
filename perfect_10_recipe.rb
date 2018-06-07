1ae219b1aa53c83f03e6d622ae5dbbebc985d345
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-07 16:28:50 +0000

    automatically backed up by learn

--- perfect_10_recipe.rb
+++ perfect_10_recipe.rb
@@ -1,5 +1,5 @@
 def perfect_10_recipe 
-  ingredients the cookie={
+  perfect_10_recipe={
   almond flour => 2 cups
    gluten freewhole oats => 1 cup
    kosher salt => 1/2 tea spoon
@@ -10,5 +10,14 @@ def perfect_10_recipe
  mini dark chocolate chips => 3/4 cup
 olive oil => 1/2 cup
 agave => 1/4 cup
-  }
+}
+end
+
+student_ages = {
+  :victoria => 25,
+  :arel => 27,
+  :carley => 25,
+  :jordan => 28, 
+  :al => 26
+}
 end
